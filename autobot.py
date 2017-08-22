48099839b49fc4317ab2c8f109879d1c49821b13
miserableoracle <miserableoracle@gmail.com>
2017-08-22 20:35:49 +0530

    Update 2
    Add
    1. is_main_post() check
    2. already_commented() check for author_m

--- autobot.py
+++ autobot.py
@@ -7,7 +7,7 @@ from steem.post import Post
 from steem.steemd import Steemd
 from steem.transactionbuilder import TransactionBuilder
 from steembase.operations import Comment
-import random
+import random, threading
 import re
 import time
 import sys
@@ -25,8 +25,8 @@ author_m = os.environ.get('Author')
 steem = Steem(keys = steemPostingKey)
 # for debugging with single poster on steemit
 debug_acc = os.environ.get('DebugAuthor')
-replyString = ""
-flag = 0
+#replyString = ""
+#flag = 0
 
 # **************************************
 # ************* FUNCTIONS **************
@@ -61,61 +61,100 @@ def TransBuilder(comment, author, cbody):
 #This function will get all the posts from given category(tag) and print top 3 posts from "created", "hot", and "trending" sections of that category(tag)
 #Argument: Category str
 
-def printposts(cat):
-	global replyString
-	global steem
-	global flag
-	createdposts = steem.get_posts(limit=3, sort="created", category=cat)
-	hotposts = steem.get_posts(limit=3, sort="hot", category=cat) 
-	trendingposts = steem.get_posts(limit=3, sort="trending", category=cat)
-	i = 0
+class printposts(threading.Thread):
+	def __init__(self, cat, comment_t):
+		threading.Thread.__init__(self)
+		self.cat_c = cat
+		self.comment = comment_t
+		flag = 0
+		global steem
+		global debug_acc
+		global author_m
+	
+	def run(self):
+		self.prepReply()
 
-	if len(createdposts) >= 1:
-		replyString += "<h3>Top 3 Recently Created Posts</h3>"
-		for i in range(0, len(createdposts)):
-			firstpost = createdposts[i]
-			postLink = "https://steemit.com/@"+firstpost["author"]+"/"+firstpost["permlink"]
-			print("https://steemit.com/@%s/%s" % (firstpost["author"], firstpost["permlink"]))
-			replyString += "<br/><b>#"+str(i+1)+".</b> <a href='"+postLink+"' target='_blank'>"+firstpost["title"]+"</a> | by @"+firstpost["author"]
-			if i == 3:
-				break
-	else:
-		print("No posts in given tag")
-		flag = 1
-	
-	replyString += "<hr/>"
-	
-	if len(hotposts) >= 1:
-		replyString += "<h3>Top 3 Hot Posts</h3>"
-		for i in range(0, len(hotposts)):
-			firstpost = hotposts[i]
-			postLink = "https://steemit.com/@"+firstpost["author"]+"/"+firstpost["permlink"]
-			print("https://steemit.com/@%s/%s" % (firstpost["author"], firstpost["permlink"]))
-			replyString += "<br/><b>#"+str(i+1)+".</b> <a href='"+postLink+"' target='_blank'>"+firstpost["title"]+"</a> | by @"+firstpost["author"]
-			if i == 3:
-				break
-	else:
-		print("No posts in given tag")
-		flag = 1
-	
-	replyString += "<hr/>"
-	
-	if len(trendingposts) >= 1:
-		replyString += "<h3>Top 3 Trending Posts</h3>"
-		for i in range(0, len(trendingposts)):
-			firstpost = trendingposts[i]
-			postLink = "https://steemit.com/@"+firstpost["author"]+"/"+firstpost["permlink"]
-			print("https://steemit.com/@%s/%s" % (firstpost["author"], firstpost["permlink"]))
-			replyString += "<br/><b>#"+str(i+1)+".</b> <a href='"+postLink+"' target='_blank'>"+firstpost["title"]+"</a> | by @"+firstpost["author"]
-			if i == 3:
-				break
-	else:
-		print("No posts in given tag")
-		flag = 1
+	def prepReply(self):
+		start_time = time.time()
+		createdposts = steem.get_posts(limit=3, sort="created", category=self.cat_c)
+		hotposts = steem.get_posts(limit=3, sort="hot", category=self.cat_c) 
+		trendingposts = steem.get_posts(limit=3, sort="trending", category=self.cat_c)
 		
-	replyString += "<hr/>"
-	replyString += "<sub> I'm a bot, beep boop | Inspired By <a href='https://www.reddit.com/user/sneakpeekbot/' target='_blank'>Reddit SneakPeekBot</a> | Recreated By @miserableoracle"
+		print("CAT (prepReply): %s" % self.cat_c)
+		replyString = ""
+		flag = 0
+		strList = ["And Roger was crazy with his bots and everything.",
+			"Bots... I think that is a hot topic.",
+			"We're fascinated with bots because they are reflections of ourselves.",
+			"Our worst comes out when we behave like bots or professionals.",
+			"Kids love robots. They're this fanciful, cool thing.",
+			"In the twenty-first century, the robot will take the place which slave labor occupied in ancient civilization.",
+			"I'm such a robot when it comes to work."
+		]
+		initStr = random.randint(0, 6)
+		replyString += "<center><em>"+strList[initStr]+"</em></center>"
+	
+		print("Received Category(Tag): %s" % cat)
+		replyString += "<h2> Here's a sneak peek of #"+cat+" posts</h2>"
 
+		i = 0
+
+		if len(createdposts) >= 1:
+			replyString += "<h3>Top "+str(len(createdposts))+" Recently Created Posts</h3>"
+			for i in range(0, len(createdposts)):
+				firstpost = createdposts[i]
+				postLink = "https://steemit.com/@"+firstpost["author"]+"/"+firstpost["permlink"]
+				print("https://steemit.com/@%s/%s" % (firstpost["author"], firstpost["permlink"]))
+				replyString += "<br/><b>#"+str(i+1)+".</b> <a href='"+postLink+"' target='_blank'>"+firstpost["title"]+"</a> | by @"+firstpost["author"]
+				if i == 3:
+					break
+		else:
+			print("No posts in given tag")
+			flag = 1
+	
+		replyString += "<hr/>"
+	
+		if len(hotposts) >= 1:
+			replyString += "<h3>Top "+str(len(hotposts))+" Hot Posts</h3>"
+			for i in range(0, len(hotposts)):
+				firstpost = hotposts[i]
+				postLink = "https://steemit.com/@"+firstpost["author"]+"/"+firstpost["permlink"]
+				print("https://steemit.com/@%s/%s" % (firstpost["author"], firstpost["permlink"]))
+				replyString += "<br/><b>#"+str(i+1)+".</b> <a href='"+postLink+"' target='_blank'>"+firstpost["title"]+"</a> | by @"+firstpost["author"]
+				if i == 3:
+					break
+		else:
+			print("No posts in given tag")
+			flag = 1
+	
+		replyString += "<hr/>"
+	
+		if len(trendingposts) >= 1:
+			replyString += "<h3>Top "+str(len(trendingposts))+" Trending Posts</h3>"
+			for i in range(0, len(trendingposts)):
+				firstpost = trendingposts[i]
+				postLink = "https://steemit.com/@"+firstpost["author"]+"/"+firstpost["permlink"]
+				print("https://steemit.com/@%s/%s" % (firstpost["author"], firstpost["permlink"]))
+				replyString += "<br/><b>#"+str(i+1)+".</b> <a href='"+postLink+"' target='_blank'>"+firstpost["title"]+"</a> | by @"+firstpost["author"]
+				if i == 3:
+					break
+		else:
+			print("No posts in given tag")
+			flag = 1
+		
+		replyString += "<hr/>"
+		replyString += "<sub> I'm a bot, beep boop | Inspired By <a href='https://www.reddit.com/user/sneakpeekbot/' target='_blank'>Reddit SneakPeekBot</a> | Recreated By @miserableoracle"
+	
+		if (self.comment["author"] == debug_acc) and (flag == 0):
+			print("REPLY IN PROGRESS")
+			self.comment.reply(replyString, '', author=author_m, meta=None)
+		elif (flag == 1):
+			print("No posts found in mentioned tag. Skip the comment.")
+		else:
+			print("Out of testing phase")
+
+		print("PROCESS FINISH TIME: %s" % (time.time() - start_time))
+		#os._exit(0)
 # **************************************
 # ************ MAIN FUNC ***************
 # **************************************
@@ -126,46 +165,44 @@ if __name__ == "__main__":
 		# Certain posts are receiving "PostDoesNotExist" exeption. Yet to find out the reason.
 		try: 
 			for comment in steem.stream_comments():
+				print("NEED TO CHECK : https://steemit.com/@%s/%s" % (comment["author"], comment["permlink"]))
 				match = re.search(r'(?i)(#)(\w+)[\w-]+', comment["body"])
 				if match is None:
 					continue
 				else:
+					# Check if the author of the post is sneakpeek bot, if TRUE ignore the rest of the part and go to next iteration
 					if (comment["author"] == author_m):
 						continue
-					replyString = ""
-					flag = 0
-					strList = ["And Roger was crazy with his bots and everything.",
-								"Bots... I think that is a hot topic.",
-								"We're fascinated with bots because they are reflections of ourselves.",
-								"Our worst comes out when we behave like bots or professionals.",
-								"Kids love robots. They're this fanciful, cool thing.",
-								"In the twenty-first century, the robot will take the place which slave labor occupied in ancient civilization.",
-								"I'm such a robot when it comes to work."
-					]
-					initStr = random.randint(0, 6)
-					replyString += "<em>"+strList[initStr]+"</em>"
-					print("https://steemit.com/@%s/%s" % (comment["author"], comment["permlink"]))
+					# Check if the comment is main post, if TRUE ignore the rest of the part and go to next iteration
+					if (comment.is_main_post()):
+						print("This is a main post. Ignore.")
+						continue
+					
+					print("MATCHED: https://steemit.com/@%s/%s" % (comment["author"], comment["permlink"]))
 					temp = match.group(0)
 					cat = temp.replace("#", "")
 					if not cat:
 						continue
 					else:
-						print("Received Category(Tag): %s" % cat)
-						replyString += "<h2> Here's a sneak peek of #"+cat+" tagged posts</h2>"
-						printposts(cat)
-						# For testing purposes
-						if (comment["author"] == debug_acc) and (flag == 0):
-						# Deploy
-						#if (flag == 0):
-							replyid = "@"+comment["author"]+"/"+comment["permlink"]
-							comment.reply(replyString, '', author=author_m, meta=None)
-						# For testing purposes	
-						elif flag == 1:
-						# Deploy
-						#else:
-							print("No posts found in mentioned tag. Skip the comment.")
-						else:
-							print("Out of testing phase")
+						#Check whether the sub-comments already include the comment from sneakpeek bot, if TRUE ignore the rest of the part and go to next iteration
+						t_ignore = 0
+						sub_comm_list = steem.get_content_replies(comment["author"], comment["permlink"])
+						for subcoms in sub_comm_list:
+							if (subcoms['author'] == author_m):
+								print("SneakPeek Bot comment already present. Ignore and go to next iteration")
+								t_ignore = 1
+								break
+								
+						if (t_ignore == 1):
+							continue
+						#newpid = os.fork()
+						print("CAT (Func-MAIN): %s" % cat)
+						# Threading is required in order to make sure
+						# that main loop doesn't miss a single block on the blockchain 
+						# while processing the current match. 
+						# Time processing traces added
+						nthread = printposts(cat, comment)
+						nthread.start()	
 		except:
-			print("Unexpected error (Sleep for 3) : ", sys.exc_info()[0])
-			time.sleep(3)
+			print("Unexpected error (Sleep for 3 (not sleeping temp)) : ", sys.exc_info()[0])
+			#time.sleep(3)
